16f04c77f527a3e8358db890612d3d760a429f6d
Felipe Lima <flf.felipe@gmail.com>
2016-10-19 23:04:52 -0200

    Configuração Log4j
    O commit inclui o Log4j 1.2.17 e configura o nível
    de log.

--- lowercase/src/main/java/br/com/challenges/lowercase/main/Main.java
+++ lowercase/src/main/java/br/com/challenges/lowercase/main/Main.java
@@ -5,12 +5,16 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 
-public class Main {
+import org.apache.log4j.Logger;
 
+public class Main {
+	//TODO verificar o logger
+	private static final Logger logger = Logger.getLogger(Main.class);
+	
 	public static void main(String[] args) {
 		try {
 			String fileName = args[0];
-			
+			logger.info("teste");
 			File file = new File(fileName);
 			BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
 		} catch (FileNotFoundException e) {
